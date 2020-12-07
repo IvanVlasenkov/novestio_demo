9ee48fe057d6dbe64f1e1daa63dd7412f45a3132
IvanVlasenkov <45207138+IvanVlasenkov@users.noreply.github.com>
2020-12-07 13:08:18 +0300

    Add files via upload
    logo in top_nav

---
+++ landing.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'cadr_1_1' => 'Novestio',
+    'cadr_1_2' => 'Mit mathematisch präziser Intelligenz optimal investieren',
+    'cadr_1_3' => 'Mit Novestio stützen Unternehmen ihre Investitionsentscheide und optimieren ihre Planung. Sie steigern ihren Profit aus Investitionen in Anlagevermögen.',
+    'cadr_1_4' => 'Entscheide trifft der Mensch – Novestio unterstützt ihn beim Finden der optimalsten Investition.',
+    'cadr_1_5' => 'Möchten Sie Novestio testen',
+
+    'cadr_2_1' => 'Novestio',
+    'cadr_2_2' => 'erlaubt hoch komplexe Was-wäre-wenn-Analysen in wenigen Minuten. Nach Erstellung eines Datenmodells berechnet Novestio mit Hilfe hoch entwickelter Modelle und Technologien Investitionsprojekte jeder Art – schnell, präzise und mit Einbezug aller möglichen Parameter. Auch wenn es Millionen sind.',
+    'cadr_2_3' => 'Novestio ist ein Investitionsrechner, der eine beliebige Anzahl beeinflussender Faktoren mit einbezieht. Künstliche Intelligenz, neuronale Netze und ein hocheffizienter Webservice steigern Profite und stärken die Planungssicherheit.',
+    'cadr_2_4' => 'Möchten Sie Novestio testen',
+
+    'cadr_3_1' => 'Maximieren Sie Ihren Profit aus Investitionen in Ihre Infrastruktur und andere Anlagevermögen.',
+    'cadr_3_2' => 'Treffen Sie mit Novestio fundierte Investitionsentscheidungen.',
+    'cadr_3_3' => 'Novestio ist ein innovativer Webservice für die Verwaltung des Investitionsportfolios von Unternehmen.',
+    'cadr_3_4' => 'Novestio lässt sich breit anwenden und berücksichtigt Tausende von beeinflussenden Faktoren',
+    'cadr_3_5' => 'Beispiele aus hunderten von Möglichkeiten:',
+    'cadr_3_6' => 'Novestio ist in allen Branchen nützlich, in denen Anlagevermögen optimiert werden müssen.',
+    'cadr_3_7' => 'Dazu stehen Unternehmen und Asset Managern die künstlichen neuronalen Netze von Novestio zur Verfügung.',
+
+    'cadr_4_1' => 'Optimal berechnen.',
+    'cadr_4_2' => 'Mit Novestio',
+    'cadr_4_3' => 'Optimale Parameter in einem komplexen System finden: Damit beschäftigt sich Novestio. Führende Mathematiker haben Novestio entwickelt und wenden hoch entwickelte Algorithmen zur schnellen Berechnung komplexer Aufgaben an.',
+    'cadr_4_4' => 'Komplexität heisst: Die Anzahl der Lösungsmöglichkeiten ist fast unendlich gross.',
+    'cadr_4_5' => 'Die Zeiten von Taschenrechnern sind längst vorbei. Gegen Novestio sehen Excel-Tabellen wie das Gekritzel eines Kleinkindes aus.',
+
+    'slide_1_1' => 'Gewinnmaximierung bei der Entwicklung von Kabelnetzen (z.B. Glasfaser)',
+    'slide_1_2' => 'Profitabler Ausbau von 5G-Netzen',
+    'slide_1_4' => 'Maximaler Profit aus Investitionen in Immobilien: Kauf, Miete, Unterhaltskosten',
+    'slide_1_3' => 'Filial- und Distributionsnetz optimieren mit Novestio: Unter Berücksichtigung wirtschaftlicher Risiken und finanzieller Investitionen berechnen Sie die optimale Entwicklung.',
+
+    'cadr_5_1' => 'Novestio',
+    'cadr_5_2' => 'is applied mathematics for an almost unsolvable problem: how to maximize the value of investment capital. ',
+    'cadr_5_3' => 'We do not foresee the future. But we solve a difficult business problem in a short time and calculate the most sensible and effective investment scenarios among millions of combinations.',
+    'cadr_5_4' => 'You get the best solutions based on precise mathematical calculations and with the help of artificial neural networks.',
+    'cadr_5_5' => 'With Novestio you can quickly make an informed decision on how to use your financial resources.',
+
+    'cadr_6_1' => 'Unser Angebot: Wege zur optimalen Investition',
+    'cadr_6_2' => 'Novestio löst hochkomplizierte analytischen Probleme auf zwei Wegen:',
+    'cadr_6_3' => 'Beratung:',
+    'cadr_6_4' => 'Gemeinsam mit unseren Spezialisten erstellen Sie ein passendes Datenmodell und lernen den Gebrauch unseres Webservices.',
+    'cadr_6_5' => 'Novestio Webservice:',
+    'cadr_6_6' => 'Experten im Bereich des Neuronalen Netzwerktrainings und der Künstlichen Intelligenz entwickelten den Webservice, um superkomplizierte Geschäftsaufgaben mit korrekten mathematischen Berechnungen zu lösen – das ist unser Fachgebiet.',
+    'cadr_6_7' => 'Ihre Vorteile gegenüber klassischen Berechnungsmethoden:',
+    'cadr_6_8' => 'Erfahrenes Team aus Beratern (Mathematik, Forschung, Businessprozesse)',
+    'cadr_6_9' => 'Verschiedene Berechnungsalgorithmen je nach Aufgabe',
+    'cadr_6_10' => '100 % Abdeckung der möglichen Kombinationen zur Auswahl des optimalen Investitionsszenarios',
+    'cadr_6_11' => '98% bis 100% Problemlösungseffizienz',
+    'cadr_6_12' => 'Herkömmliche Methoden sind unzuverlässig, dauern oft bis zu mehreren Monaten;',
+    'cadr_6_13' => 'Asset Manager erreichen damit jedoch nur mehr als 80 % Problemlösungseffizienz.',
+
+    'cadr_7_1' => 'Technologie für die digitalisierte Wirtschaft',
+    'cadr_7_2' => 'Novestio hat mehrere hochentwickelte und verifizierte Algorithmen im Einsatz. Diese ermöglichen es, ein Investitionsproblem in kurzer Zeit zu lösen. Dabei unterstützen wir Sie gerne. Wenn nötig, integrieren wir unseren Service in ihre bestehende Softwarelandschaft.',
+    'cadr_7_3' => 'Novestio löst Probleme mit mehreren tausend Assets und einer Komplexität mit tausenden von Einflussfaktoren.',
+
+    'cadr_8_1' => 'Novestio bedeutet: schnelle Berechnung von maximalen Return-on-Investment-Lösungen für Business jeder Größenordnung und mit jedem Budget.',
+    'cadr_8_2' => 'Mit Novestio optimieren Unternehmen ihren Einsatz der Finanzmittel, verbessern ihre Margen, stärken ihre Innovationskraft und sind damit unter anderem auf instabile Märkte, Krisen und andere Ereignisse besser vorbereitet.',
+    'cadr_8_3' => 'Novestio – das sind bewährte Algorithmen für neuronale Netzwerke, um alle Optionen zu analysieren und die besten Lösungen für Ihr Business auszuwählen:',
+    'cadr_8_4' => 'Wir schätzen Ihre Zeit und verarbeiten Daten schnell',
+    'cadr_8_5' => 'Wir führen parallele Berechnungen auf Videokarten (CUDA-Kerne) durch',
+    'cadr_8_6' => 'Wir integrieren Dienstleistungen in das Firmen-Ökosystem',
+    'cadr_8_7' => 'Wir unterstützen den Systembetrieb, verbessern die Genauigkeit und Geschwindigkeit der Berechnungen für Ihre Aufgaben.',
+
+    'cadr_9_1' => 'Kontakt',
+    'cadr_9_2' => 'Novestio ist ein Brand von Reset Thinking, dem Development- und Innovationslabor der Schweiz.',
+    'cadr_9_3' => 'Sie erreichen unsere Experten per E-Mail',
+    'cadr_9_4' => 'hello@novest.io',
+    'cadr_9_5' => 'Legal Information',
+    'cadr_9_6' => 'Novestio ist ein Webservice zur schnellen Berechnung komplexer Herausforderungen bei der Investition in Anlagevermögen.',
+    'cadr_9_7' => 'Es handelt sich nicht um ein Expertensystem, das Entscheidungen trifft.',
+    'cadr_9_8' => 'Novestio unterstützt Asset Manager bei der optimalen Investition in Anlagevermögen.',
+    'cadr_9_9' => 'Je ausgefeilter das zugrunde liegende Datenmodell, desto präziser und nutzbringender sind die Resultate.',
+    'cadr_9_10' => 'Novestio-Experten unterstützen gerne bei der Erstellung des Datenmodells und bei der Nutzung des Services.',
+    'cadr_9_11' => 'Novestio übernimmt keine Garantie für die Qualität der eingesetzten Daten, jedoch für die Qualität der eingesetzten Algorithmen und des neuronalen Netzwerks.',
+
+    'cadr_10_1' => 'Testen Sie Novestio',
+    'cadr_10_2' => 'Novestios Webservice können Sie gefahrlos selbst ausprobieren. Beachten Sie dabei: Verwenden Sie für diesen Testservice keine sensiblen Daten. Novestio berechnet die optimalsten Werte als Grundlage für Ihren Entscheid – den Sie selbst treffen.',
+    'cadr_10_3' => 'Unsere Experten stehen Ihnen für Ihre Fragen und Wünsche zur Verfügung und leiten Sie auch bei der Nutzung des Webservices gerne an:',
+    'cadr_10_4' => 'Interessiert? - Melden Sie sich an!',
+    'cadr_10_5' => 'Neuer Benutzer',
+    'cadr_10_6' => 'Anmelden',
+    'cadr_10_7' => 'Benutzername/Passwort vergessen?',
+    'cadr_10_8' => 'helpme@novest.io',
+
+
+    'cadr_11_1' => 'Haben Sie noch Fragen?',
+    'cadr_11_2' => 'Name',
+    'cadr_11_3' => 'Telefonnummer',
+    'cadr_11_4' => 'Email',
+    'cadr_11_5' => 'Senden',
+    'cadr_11_6' => 'Ich stimme dem Datendaten-System zu.',
+
+    'footer_1' => 'Registrieren',
+    'footer_2' => 'Einloggen',
+    'footer_3' => 'Datenschutzerklärung',
+
+    'footer_office_ru' => 'Büro in Russland:',
+    'footer_office_de' => 'Büro in Deutschland:',
+    'footer_office_sv' => 'Büro in der Schweiz:',
+    'footer_office_ru_adress' => '74 Kruglikovskaya Straße, Krasnodar, 350059',
+    'footer_office_de_adress' => '95 Liebenauer Straße, Worms, 67549',
+    'footer_office_sv_adress' => '1 Wiesenstrasse, Langenthal, 4900',
+
+];
---
+++ messages.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'w10' => 'Varianten',
+    'aside_w1' => 'Main',
+    'aside_w2' => 'Optionen',
+
+    'w_panel_1' => 'Selbständig',
+    'w_panel_2' => 'Objekttypen',
+    'w_panel_3' => 'Anzahl der Objekte',
+    'w_panel_4' => 'Start',
+    'logged' => 'Sie sind eingeloggt!',
+    'dashboard' => 'Dashboard',
+    'demo' => 'Möglichkeiten',
+    'start' => 'Mach es selbst',
+    'profi' => 'Profi',
+    'helpme' => 'Help me',
+    'vip' => 'All inclusive',
+    'youname' => 'Ihr Name',
+    'mail' => 'E-mail',
+    'call_me' => 'Rufen mir zurück an',
+    'profile' => 'Profil',
+    'gallery' => 'Galerie',
+
+    'morethen300' => 'über 300 Knoten',
+    'from50to300' => 'von 50 bis 300 Knoten',
+    'from15to50' => 'von 15 bis 30 Knoten',
+    'to15' => 'bis 15 Knoten',
+    
+
+    'variant' => 'Berechnungsoption',
+    'page_vip_w1' => 'Individuell gefertigt.',
+    'page_vip_w2' => 'Überlassen Sie Ihre Kontakte und wir werden Sie auf jeden Fall zurückrufen.',
+
+    'page_profi_w1' => 'Automatische Suche nach der besten Option',
+    'page_profi_w2' => 'Verwendung berechnet auf CUDA-Kernen',
+    'page_profi_w3' => 'In Entwicklung',
+
+    'page_start_w1' => 'Testversion des Webservices',
+    'page_start_w2' => 'Testen Novestio',
+
+    'page_demo_w1' => 'Machen Sie sich bekannt mit Novestio',
+
+];
